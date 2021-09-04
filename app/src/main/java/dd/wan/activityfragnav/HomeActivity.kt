13ef4a45f41d07fc078eca1c9fbb87d2589fee1b
DoanNam2000 <doanduynam2000@gmail.com>
2021-09-04 14:21:16 +0700

    1 step to finish

--- app/src/main/java/dd/wan/activityfragnav/HomeActivity.kt
+++ app/src/main/java/dd/wan/activityfragnav/HomeActivity.kt
@@ -1,23 +1,68 @@
 package dd.wan.activityfragnav
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.Window
+import android.view.WindowManager
 import androidx.navigation.NavController
-import androidx.navigation.Navigation
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
-import dd.wan.activityfragnav.fragment.*
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.custom_dialog.*
+
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var navHostFragment:NavHostFragment
+    lateinit var controller: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         supportActionBar!!.hide()
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
-        val controller: NavController = navHostFragment.navController
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        controller = navHostFragment.navController
         bottomNavigationView.setupWithNavController(controller)
+    }
 
+    override fun onNavigateUp(): Boolean {
+        return controller.navigateUp() || super.onNavigateUp()
+    }
+    override fun onBackPressed() {
+        if (navHostFragment.childFragmentManager.backStackEntryCount == 0) {
+            val dialog = Dialog(this)
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+            dialog.setContentView(R.layout.custom_dialog)
+            val window = dialog.window
+            if (window != null) {
+                window.setLayout(
+                    WindowManager.LayoutParams.MATCH_PARENT,
+                    WindowManager.LayoutParams.WRAP_CONTENT
+                )
+                window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+                val windowAttribute = window.attributes
+                windowAttribute.gravity = Gravity.CENTER
+                window.attributes = windowAttribute
+
+                if (Gravity.BOTTOM == Gravity.CENTER) {
+                    dialog.setCancelable(true)
+                } else {
+                    dialog.setCancelable(false)
+                }
+                dialog.cancel.setOnClickListener {
+                    dialog.dismiss()
+                }
+                dialog.exit.setOnClickListener {
+                    dialog.dismiss()
+                    super.onBackPressed()
+                }
+            }
+            dialog.show()
+        } else {
+            super.onBackPressed()
+        }
     }
 }
--- app/src/main/java/dd/wan/activityfragnav/fragment/OnBoardingFragment.kt
+++ app/src/main/java/dd/wan/activityfragnav/fragment/OnBoardingFragment.kt
@@ -1,5 +1,6 @@
 package dd.wan.activityfragnav.fragment
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
@@ -17,33 +18,36 @@ import kotlinx.android.synthetic.main.fragment_on_boarding.view.*
 class OnBoardingFragment : Fragment() {
     lateinit var sharedPreferences:SharedPreferences
     var isRemember=false
-    override fun onAttach(context: Context) {
-        sharedPreferences = requireActivity().getSharedPreferences("SHARE_PREFERENCES", Context.MODE_PRIVATE)
-        isRemember = sharedPreferences.getBoolean("check",false)
-        if(isRemember)
-        {
-            startActivity(Intent(activity,MainActivity::class.java))
-        }
-        super.onAttach(context)
-    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        var view = inflater.inflate(R.layout.fragment_on_boarding, container, false)
+        sharedPreferences = requireActivity().getSharedPreferences("SHARE_PREFERENCES", Context.MODE_PRIVATE)
+        isRemember = sharedPreferences.getBoolean("check",false)
+        if(!isRemember) {
+            var view = inflater.inflate(R.layout.fragment_on_boarding, container, false)
 
 
-        view.btnNext.setOnClickListener {
-            findNavController().navigate(R.id.onBoarding2Fragment)
+            view.btnNext.setOnClickListener {
+                findNavController().navigate(R.id.onBoarding2Fragment)
+            }
+            view.skip.setOnClickListener {
+                sharedPreferences = requireActivity().getSharedPreferences(
+                    "SHARE_PREFERENCES",
+                    Context.MODE_PRIVATE
+                )
+                val editor: SharedPreferences.Editor = sharedPreferences.edit()
+                editor.putBoolean("check", true)
+                editor.apply()
+                startActivity(Intent(activity, MainActivity::class.java))
+            }
+            return view
         }
-        view.skip.setOnClickListener {
-            sharedPreferences = requireActivity().getSharedPreferences("SHARE_PREFERENCES", Context.MODE_PRIVATE)
-            val editor:SharedPreferences.Editor = sharedPreferences.edit()
-            editor.putBoolean("check",true)
-            editor.apply()
-            startActivity(Intent(activity,MainActivity::class.java))
+        else
+        {
+            startActivity(Intent(activity,MainActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK))
+            return null
         }
-        return  view
     }
 }
